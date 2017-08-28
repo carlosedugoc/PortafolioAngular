25e56e4b073dde765f1ede1bf96346ddb74ceb7c
carlosedugoc <carlosedugoc@gmail.com>
2017-08-28 14:05:58 -0500

    Se actualiza about

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { InformacionService } from '../..//services/informacion.service'
+import 'rxjs/Rx'
 
 @Component({
   selector: 'app-about',
@@ -7,7 +9,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
-  constructor() { }
+  constructor(private informacionService:InformacionService) {
+    this.informacionService.carga_sobre_nosotros;
+   }
 
   ngOnInit() {
   }

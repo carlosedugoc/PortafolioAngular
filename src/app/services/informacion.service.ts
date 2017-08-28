12d6ebe76556a2fb108b098e716768682cd0c727
carlosedugoc <carlosedugoc@gmail.com>
2017-08-28 13:51:00 -0500

    Servicio firebase listo

--- src/app/services/informacion.service.ts
+++ src/app/services/informacion.service.ts
@@ -4,15 +4,35 @@ import { Http } from '@angular/http'
 @Injectable()
 export class InformacionService {
   info:any = {}
+  equipo:any[] = []
   cargada:boolean = false
+  cargada_about:boolean = false
   constructor(public http:Http) { 
+    this.carga_info();
+    this.carga_sobre_nosotros();
+  }
+
+
+
+  public carga_info(){
+    
     this.http.get("assets/data/info.pagina.json")
     .subscribe( data =>{
-      console.log(data.json())
       this.cargada=true;
       this.info = data.json();
-    })
+    }
+    )
+  }
 
+  public carga_sobre_nosotros(){
+    this.http.get("https://webtemplate-ac160.firebaseio.com/equipo.json")
+    .subscribe( data =>{
+      console.log(data.json());
+      this.cargada_about=true;
+      this.equipo = data.json();
+    }
+    )
   }
 
 }  
+
